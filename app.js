350cb548fc615551304c64f055fb434925c14bba
nguyentran186 <reignoverabus@gmail.com>
2023-11-25 12:17:58 +0700

    first question

--- app.js
+++ app.js
@@ -23,24 +23,28 @@ const app = express();
 /////////////       FUNCTION                ///////////////
 ///////////////////////////////////////////////////////////
 let supplier_query = (id, req) => {
-    if (req['enableDateTimeRange'] == true){
-        console.log(1)
-        let fab_id_query = 'SELECT * FROM import_info WHERE fabcat_code = ? AND import_date >= ? and import_date <= ?';
-        db.query(fab_id_query, [id, req['dateFrom'], req['dateTo']], (err, result) => {
-        if (err) throw err;
-            console.log(result)
-            return result
-        })
-    }
-    else {
-        let fab_id_query = 'SELECT * FROM import_info WHERE fabcat_code = ?';
-        db.query(fab_id_query, id, (err, result) => {
-            if (err) throw err;
-            console.log(result)
-            return result
-        })
-    }
-}
+    return new Promise((resolve, reject) => {
+        if (req['enableDateTimeRange'] == true) {
+            let fab_id_query = 'SELECT * FROM import_info WHERE fabcat_code = ? AND import_date >= ? and import_date <= ?';
+            db.query(fab_id_query, [id, req['dateFrom'], req['dateTo']], (err, result) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        } else {
+            let fab_id_query = 'SELECT * FROM import_info WHERE fabcat_code = ?';
+            db.query(fab_id_query, id, (err, result) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        }
+    });
+};
 
 
 
@@ -52,34 +56,35 @@ app.listen('3000', () => {
     console.log('Server started on port 3000')
 });
 
-app.get('/ques1', (req , res) => {
+app.get('/ques1', (req, res) => {
+    let request = {
+        'categoryName': 'Tasar Silk',
+        'enableDateTimeRange': false,
+        'dateFrom': '2023-08-30',
+        'dateTo': '2023-09-30'
+    };
+
     let id = 'undefined';
 
     if (req['searchByID'] == true) {
         id = req['categoryID'];
-    }
-    else{
-        let name_query = 'SELECT * FROM fabric_cat WHERE name = ?'
-        db.query(name_query, 'Tasar Silk', async (err, result) => {
-        // db.query(name_query, req['categoryName'], (err, result) => {
-            if (err) throw err;
-            id = result[0]['fabcat_code'];
-            request = {
-                'enableDateTimeRange': false,
-                'dateFrom': '2023-08-30',
-                'dateTo': '2023-09-30'
+    } else {
+        let name_query = 'SELECT * FROM fabric_cat WHERE name = ?';
+        db.query(name_query, request['categoryName'], async (err, result) => {
+            if (err) {
+                throw err;
             }
-            let query_result = await supplier_query(id, request)
-            console.log(query_result)
-        })
+            id = result[0]['fabcat_code'];
+
+            try {
+                let query_result = await supplier_query(id, request);
+                console.log(query_result);
+                res.json(query_result);
+            } catch (err) {
+                console.error(err);
+                // Handle error appropriately
+            }
+        });
     }
+});
 
-
-    
-    // let sql = 'SELECT * FROM BOLT';
-    // db.query(sql, (err,result) => {
-    //     if(err) throw err;
-    //     console.log(result);
-    //     res.send(result);
-    // })
-})
