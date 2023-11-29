2849396f88b2abe83eb6554cbacd3ef0b5cd0129
Quessalini03 <vuchauduyquang@gmail.com>
2023-11-29 14:48:53 +0700

    fix: create table admin_account in login

--- mysql/login.sql
+++ mysql/login.sql
@@ -1,14 +1,14 @@
 DROP DATABASE IF EXISTS login;
 CREATE DATABASE login;
 
-CREATE TABLE admin_account (
+CREATE TABLE login.admin_account (
 	username varchar(30),
     password varchar(30),
     PRIMARY KEY (username)
 );
 
-INSERT INTO admin_account(username, password) values 
+INSERT INTO login.admin_account(username, password) values 
 ('admin1', '123456'),
 ('admin2', '123456');
 
-SELECT * FROM admin_account;
+SELECT * FROM login.admin_account;
