fed23566bc802b578d4b6efc54d3cbd58e0ac866
Quessalini03 <vuchauduyquang@gmail.com>
2023-12-06 22:51:32 +0700

    feat: add hasing to login

--- login.js
+++ login.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql')
+const bcrypt = require('bcrypt');
 
 const db = mysql.createConnection({
     host: 'localhost',
@@ -12,18 +13,43 @@ const db = mysql.createConnection({
 // ##############################################
 // ##############################################
 
-const safeLogin = (req) => {
-  return new Promise((resolve, reject) => {
-    const query = 'SELECT * FROM admin_account WHERE username = ? AND password = ?';
-    db.query(query, [req["username"], req["password"]], (err, result) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(result);
-      }
-    });
-  });
-}
+const get_username_and_hashed_password = (username) => {
+    return new Promise((resolve, reject) => {
+        const query = 'SELECT * FROM admin_account_hash WHERE username = ?';
+        db.query(query, [username], (err, result) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(result);
+            }
+        })
+  })}
+
+const safeLogin = async (req) => {
+  try {
+    const username = req["username"];
+    const enteredPassword = req["password"];
+
+    const result = await get_username_and_hashed_password(username);
+
+    if (result.length === 0) {
+      return [];
+    }
+
+    const storedHashedPassword = result[0]['password_hash'];
+
+    const passwordMatch = await bcrypt.compare(enteredPassword, storedHashedPassword);
+
+    if (passwordMatch) {
+      return result;
+    } else {
+      return [];
+    }
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
 
 const unsafeLogin = (req) => {
   return new Promise((resolve, reject) => {
--- mysql/login.sql
+++ mysql/login.sql
@@ -12,3 +12,16 @@ INSERT INTO login.admin_account(username, password) values
 ('admin2', '123456');
 
 SELECT * FROM login.admin_account;
+
+CREATE TABLE login.admin_account_hash (
+    username varchar(30),
+    password_hash varchar(60),
+    PRIMARY KEY (username)
+);
+
+-- Both accounts have the same password "123456"
+INSERT INTO login.admin_account_hash(username, password_hash) values 
+('admin1', '$2b$12$w1WuhV2TYbIgdXyUazsWcelBWzrMPb1o5WinLaxBfJ5mcVC0/jwA6'),
+('admin2', '$2b$12$MSPjPt6goroLC9R8DK5BIOqXUqofRxF8m4B86wSpoZ8EUpnJKgXpm');
+
+SELECT * FROM login.admin_account_hash;
